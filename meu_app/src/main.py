2ae36687e4f1833a8b6e4abc282b524f68de7b97
MacuxiBrabo <pdr.ggoncalves@gmail.com>
2026-07-08 16:47:29 -0400

    adicionei uma função para cada operação matemática e organizei os botões da calculadora

--- meu_app/src/main.py
+++ meu_app/src/main.py
@@ -5,9 +5,31 @@ def main(page: ft.Page):
     page.title = 'Minha Calculadora'
     resultado = ft.Text(value = '0')
 
-    page.add(resultado, ft.Button('AC'), ft.Button('-/+'), ft.Button('%'), ft.Button('/'), ft.Button('7'), ft.Button('8'), 
-             ft.Button('9'), ft.Button('*'), ft.Button('4'), ft.Button('5'), ft.Button('6'), ft.Button('-'), ft.Button('1'), 
-             ft.Button('2'), ft.Button('3'), ft.Button('+'), ft.Button('0'), ft.Button('.'), ft.Button('='), )
+    # operação de soma
+    def somar():
+        pass
+
+    # operação de subtração
+    def subtração():
+        pass
+
+    # operação de multiplicação
+    def multiplicacao():
+        pass
+
+    # operação de divisão
+    def divisao():
+        pass
+
+    
+    page.add(
+        ft.Row([resultado]),
+        ft.Row([ft.Button('AC'), ft.Button('-/+'), ft.Button('%'), ft.Button('/')]),
+        ft.Row([ft.Button('7'), ft.Button('8'), ft.Button('9'), ft.Button('*')]),
+        ft.Row([ft.Button('4'), ft.Button('5'), ft.Button('6'), ft.Button('-')]),
+        ft.Row([ft.Button('1'), ft.Button('2'), ft.Button('3'), ft.Button('+')]),
+        ft.Row([ft.Button('0'), ft.Button('.'), ft.Button('=')])
+    )
 
 if __name__ == '__main__':
     ft.run(main)
